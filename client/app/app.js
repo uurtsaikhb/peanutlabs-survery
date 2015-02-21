0b846d23952ecb84b0123f2d18357c3aa2485f9a
Uurtsaikh Batbileg <uurtsaikh.ba@gmail.com>
2015-02-21 21:00:02 +0000

    commit

--- client/app/app.js
+++ client/app/app.js
@@ -1,24 +1,3 @@
-/// <reference path="../Scripts/angular-1.1.4.js" />
-
-/*#######################################################################
-  
-  Dan Wahlin
-  http://twitter.com/DanWahlin
-  http://weblogs.asp.net/dwahlin
-  http://pluralsight.com/training/Authors/Details/dan-wahlin
-
-  Normally like to break AngularJS apps into the following folder structure
-  at a minimum:
-
-  /app
-      /controllers      
-      /directives
-      /services
-      /partials
-      /views
-
-  #######################################################################*/
-
 var app = angular.module('survey', ['ngRoute']);
 
 //This configures the routes and associates each route with a view and a controller
