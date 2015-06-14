0d071e1e2fa4de8c0c616fb313331ae7861858a0
Uurtsaikh <uurtsaikh.ba@gmail.com>
2015-06-13 22:34:09 -0400

    testing git from command line

--- server.js
+++ server.js
@@ -24,3 +24,4 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Survey server listening at", addr.address + ":" + addr.port);
 });
 test
+test again
