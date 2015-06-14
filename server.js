2df3498896cdbb6f6d2547f0ac73b40b3d202ccc
Uurtsaikh <uurtsaikh.ba@gmail.com>
2015-06-13 22:27:05 -0400

    getting started with git.

--- server.js
+++ server.js
@@ -20,5 +20,7 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
+  console.log('HelloWorld');
   console.log("Survey server listening at", addr.address + ":" + addr.port);
 });
+test
