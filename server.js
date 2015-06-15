6bb9b1d1a8898f81d4b140b686774441812e198b
Uurtsaikh <uurtsaikh.ba@gmail.com>
2015-06-15 14:50:55 -0400

    added display method on branch server

--- server.js
+++ server.js
@@ -24,3 +24,7 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Survey server listening at", addr.address + ":" + addr.port);
 });
 
+function display (data){
+	console.log('imported data: '+ data);
+}
+
