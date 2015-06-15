371acfc2a86359f2f342d20b45d0ccd62cf3f30d
Uurtsaikh <uurtsaikh.ba@gmail.com>
2015-06-15 14:41:44 -0400

    deleted extra lines from end of file.

--- server.js
+++ server.js
@@ -23,6 +23,4 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log('HelloWorld');
   console.log("Survey server listening at", addr.address + ":" + addr.port);
 });
-test
-test again
-again one line
+
