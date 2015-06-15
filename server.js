896eca4a36279e7daab4d0c9ee796bc979582493
Uurtsaikh <uurtsaikh.ba@gmail.com>
2015-06-15 14:47:29 -0400

    added a method in server.js

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log('HelloWorld');
   console.log("Survey server listening at", addr.address + ":" + addr.port);
 });
+
+
+
+var display = function (data){
+	console.log('inserted data: ' + data.data);
+}
 
